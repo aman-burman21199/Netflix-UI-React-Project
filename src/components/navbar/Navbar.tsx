52c90bdaa9704cc27c6c8650a767522fe78b7637
Aman Burman <amanb21199@gmail.com>
2023-12-14 17:43:31 +0530

    Netflix Navbar Part 3 - Final

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,10 +2,23 @@ import { Search, Notifications, ArrowDropDown } from "@material-ui/icons"
 import "./navbar.scss"
 import ProfilePic from "../../assets/Profile Pic.jpg"
 import NetflixLogo from "../../assets/Netflix-Logo.png"
+import { useState } from "react"
 
 const Navbar: React.FC = () => {
+  // State to change background of Navbar while scrolling.
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  window.onscroll = () => {
+    setIsScrolled(window.scrollY === 0 ? false : true)
+
+    // Cleanup function otherwise it will run in an infinite loop.
+    return () => (window.onscroll = null)
+  }
+
+  console.log(isScrolled)
+
   return (
-    <div className='navbar'>
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className='container'>
         <div className='left'>
           <img src={NetflixLogo} alt='Netflix Logo' />
