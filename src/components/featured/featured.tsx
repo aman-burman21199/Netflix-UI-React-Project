21798526d5efa4350181bd7f587de5562a39df8c
Aman Burman <amanb21199@gmail.com>
2023-12-15 21:18:40 +0530

    Featured Movie Part-1

---
+++ src/components/featured/featured.tsx
@@ -0,0 +1,37 @@
+import { InfoOutlined, PlayArrow } from "@material-ui/icons"
+import "./featured.scss"
+
+const Featured: React.FC = () => {
+  return (
+    <div className='featured'>
+      <img
+        src='https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+        alt='Featured Image'
+      />
+      <div className='info'>
+        <img
+          src='https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1'
+          alt=''
+        />
+        <span className='desc'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et,
+          blanditiis quibusdam non voluptatum minima dicta excepturi itaque
+          tempore esse veritatis libero quidem porro repellat temporibus ut
+          aspernatur numquam qui eligendi?
+        </span>
+        <div className='buttons'>
+          <button className='play'>
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className='more'>
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
